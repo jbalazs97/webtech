123af87379145ccebaa5cd05a1c79691413cb966
Balázs Juhász <juhaszbalazs855@gmail.com>
2021-11-26 14:06:26 +0100

    done

---
+++ validation.js
@@ -0,0 +1,66 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Nem maradhat üresen');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Nem maradhat üresen');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Nem valós e-mail');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Nem maradhat üresen');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Nem maradhat üresen');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'A jelszavak nem egyeznek');
+	} else{
+		setSuccessFor(password2);
+	}
+	if (document.getElementById("agree").checked == false){
+        valid = false;
+        errorMessage.push("Regulations are not accepted\n");
+    }
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
